fd13dbe02e7e31791085ff7b1322e2cee2facb2a
MoritzHuppert <35614875+MoritzHuppert@users.noreply.github.com>
2023-11-23 10:03:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Paths to the STP and cryptominisat executable
-PATH_STP = "./Cryptanet/extern/stp/build/stp"
-PATH_CRYPTOMINISAT = "./Cryptanet/extern/cryptominisat/build/cryptominisat5"
-PATH_BOOLECTOR = "./Cryptanet/extern/boolector/build/bin/boolector"
+PATH_STP = "./Mercy/extern/stp/build/stp"
+PATH_CRYPTOMINISAT = "./Mercy/extern/cryptominisat/build/cryptominisat5"
+PATH_BOOLECTOR = "./Mercy/extern/boolector/build/bin/boolector"
 #Maximum weight for characteristics to search for
 MAX_WEIGHT = 1000
 #Maximum number of characteristics to search for a differential
